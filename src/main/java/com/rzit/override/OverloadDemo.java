d5e1ba1f2be6a76b21432d415bcb6e71cb4e7600
RanjithTeche <ranjithteche@gmail.com>
2017-09-14 23:20:26 +0530

    Create OverloadDemo.java

---
+++ src/main/java/com/rzit/override/OverloadDemo.java
@@ -0,0 +1,31 @@
+ class Overload {
+    public void p(int a){
+        System.out.println("int:"+a);
+    }
+    public void p(Integer a){
+        System.out.println("Integer:"+a);
+    }
+    public void p(Object a){
+        System.out.println("Object:"+a);
+    }
+    /*public void p(String a){
+        System.out.println("String:"+a);
+    }*/
+    public void p(long a){
+        System.out.println("long:"+a);
+    }
+}
+
+
+public class OverloadDemo {
+    public static void main(String[] args) {
+    
+        Overload obj= new Overload();
+        obj.p(10);
+        obj.p(null);
+        Object oo= null;
+        obj.p(oo);
+        obj.p(new Integer(10));
+    }
+    
+}
